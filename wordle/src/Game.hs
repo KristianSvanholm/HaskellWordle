4abae5a66990a012f623e64c8201dafa3f777993
Kristian Røren Svanholm <krisrsva@ntnu.no>
2022-04-19 13:58:49 +0200

    Main game loop added. Reacts appropriately given an input.

--- wordle/src/Game.hs
+++ wordle/src/Game.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Game where
 
 find:: String -> [String] -> Bool
@@ -11,3 +12,23 @@ checkWord a g w
     | a == g = 0 -- Win condition
     | find g w = 1 -- Word exists, but is not correct word.
     | otherwise = 2 -- Word does not exist
+
+game:: String -> [String] -> [String]-> IO ()
+game a w gameState = do
+    if att /= 6 then do
+        putStrLn "New attempt:"
+
+        input <- getLine
+        let cond = checkWord a input w
+
+        case cond of
+            0 -> putStrLn "You win!"
+            1 -> game a w (gameState++[input])
+            2 -> do 
+                putStrLn "Not a word!"
+                game a w gameState
+    else do
+        putStrLn "Out of attempts!"
+
+    where
+        att = length gameState
