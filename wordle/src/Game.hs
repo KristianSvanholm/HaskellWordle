0e3403319c48b586477d2f8bb71f9d082a6421ec
Kristian Røren Svanholm <krisrsva@ntnu.no>
2022-04-19 13:51:32 +0200

    Function checkWord. Returns either 0,1 or 2 dependent on the correctness of a guessed word. 0 = win 1 = Exists, but not correct 2 = Does not exist.

--- wordle/src/Game.hs
+++ wordle/src/Game.hs
@@ -5,3 +5,9 @@ find _ [] = False
 find n (x:xs)
     | x == n = True
     | otherwise = find n xs
+
+checkWord:: String -> String -> [String] -> Int
+checkWord a g w
+    | a == g = 0 -- Win condition
+    | find g w = 1 -- Word exists, but is not correct word.
+    | otherwise = 2 -- Word does not exist
