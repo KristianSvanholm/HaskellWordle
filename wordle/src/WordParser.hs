4f00b93c237b7880fd8714262b68cee83b456926
Kristian Svanholm <KristianRorenSvanholm@gmail.com>
2022-04-20 17:05:15 +0200

    Added parse function. Generate a tuple per letter with info about the letters accuracy.

--- wordle/src/WordParser.hs
+++ wordle/src/WordParser.hs
@@ -1,1 +1,12 @@
 module WordParser where
+import Data.List
+
+parse :: String -> Int -> String -> [(Char,Int)]
+parse [] _ _ = []
+parse (l:gs) n a = checkLetter l n a : parse gs (n+1) a
+
+-- TODO: IMPLEMENT DOUBLE YELLOW CHECK SOMEHOW
+checkLetter :: Char -> Int -> String -> (Char,Int)
+checkLetter l n a
+    | l `elem` a = if l == a!!n then (l,2) else (l,1)
+    | otherwise = (l,0)
